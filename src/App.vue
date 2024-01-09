19c99fe5183ff1d97bf032743f870fe6dd4c8a78
osmarpj <osmar.pj@hotmail.com>
2024-01-09 16:32:30 -0500

    UPLOAD first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import {ref} from 'vue'
+const msg = ref('')
+</script>
+
+<template>
+  <div>
+    <input v-model="msg" type="text"/>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
