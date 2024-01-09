bc91a695dd637a8a53facb9e09f3e6e2f58fcfea
JuanRMachucaBarzola <70106289@continental.edu.pe>
2024-01-09 17:02:56 -0500

    UPDATE - Formulario actualizado

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <script setup>
-import {ref} from 'vue'
-const msg = ref('')
+import { ref } from "vue";
+const msg = ref("");
 </script>
 
 <template>
   <div>
-    <input v-model="msg" type="text"/>
+    <input v-model="msg" type="text" class="input-class" />
     <h1>{{ msg }}</h1>
   </div>
 </template>
 
 <style scoped>
-
+.input-class {
+  max-width: 300px;
+  width: 100%;
+  height: 45px;
+  background-color: #eeeeee;
+  color: black;
+  border-radius: 5px;
+  padding: 12px 14px;
+}
 </style>
