2654646219855f1be07cd5e45ff5ae13a260e814
JuanRMachucaBarzola <70106289@continental.edu.pe>
2024-01-09 17:30:49 -0500

    UPDATE - Estilos generales

--- src/App.vue
+++ src/App.vue
@@ -4,21 +4,31 @@ const msg = ref("");
 </script>
 
 <template>
-  <div>
-    <input v-model="msg" type="text" class="input-class" />
-    <h1>{{ msg }}</h1>
+  <div class="login-container">
+    <div class="login-header"></div>
+    <div class="login-boy">
+      <input v-model="msg" type="text" class="input-class" />
+      <!-- <h1>{{ msg }}</h1> -->
+    </div>
+    <div class="lo"></div>
   </div>
 </template>
 
 <style scoped>
+.login-container {
+  background-color: white;
+  max-width: 500px;
+  width: 100%;
+  padding: 1rem;
+  border-radius: 1rem;
+}
 .input-class {
   max-width: 300px;
   width: 100%;
-  height: 45px;
+  height: 25px;
   background-color: grey;
   color: black;
-  border-radius: 5px;
+  border-radius: 8px;
   padding: 12px 14px;
-
 }
 </style>
