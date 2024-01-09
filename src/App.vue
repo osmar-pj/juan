6fd77f4ddc41fbda8ec80e241ebd713ea6e3d001
JuanRMachucaBarzola <70106289@continental.edu.pe>
2024-01-09 17:40:49 -0500

    UPDATE - Incluyendo sass

--- src/App.vue
+++ src/App.vue
@@ -8,27 +8,27 @@ const msg = ref("");
     <div class="login-header"></div>
     <div class="login-boy">
       <input v-model="msg" type="text" class="input-class" />
-      <!-- <h1>{{ msg }}</h1> -->
+       <h1>{{ msg }}</h1> 
     </div>
     <div class="lo"></div>
   </div>
 </template>
 
-<style scoped>
+<style lang="scss">
 .login-container {
-  background-color: white;
+  background-color: var(--grey-light);
   max-width: 500px;
   width: 100%;
   padding: 1rem;
   border-radius: 1rem;
-}
-.input-class {
-  max-width: 300px;
-  width: 100%;
-  height: 25px;
-  background-color: grey;
-  color: black;
-  border-radius: 8px;
-  padding: 12px 14px;
+  .input-class {
+    max-width: 300px;
+    width: 100%;
+    height: 25px;
+    background-color: var(--grey-light);
+    color: black;
+    border-radius: 8px;
+    padding: 12px 14px;
+  }
 }
 </style>
